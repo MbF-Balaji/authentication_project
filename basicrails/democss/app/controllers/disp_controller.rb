1437c6b73026789905746014b0b8e7c7aa68be7a
balambf <balaji.d@madebyfire.com>
2017-03-08 15:39:03 +0530

    adding css to the html page

---
+++ basicrails/democss/app/controllers/disp_controller.rb
@@ -0,0 +1,5 @@
+class DispController < ApplicationController
+	def show
+		
+	end
+end
---
+++ basicrails/democss/test/controllers/disp_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DispControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
