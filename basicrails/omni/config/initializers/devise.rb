c9ab7a63b1a2dd195438e2583fae6ac2d1cbfb67
balambf <balaji.d@madebyfire.com>
2017-03-15 12:17:16 +0530

    pdated_auth

--- basicrails/omni/config/initializers/devise.rb
+++ basicrails/omni/config/initializers/devise.rb
@@ -1,5 +1,5 @@
 Devise.setup do |config| #Replace example.com with your own domain name
-    config.mailer_sender = 'http://localhost:3000'
+    config.mailer_sender = 'balaji.d.94.it@gmail.com'
     require 'devise/orm/active_record'
     config.case_insensitive_keys = [ :email ]
     config.strip_whitespace_keys = [ :email ]
@@ -10,6 +10,8 @@ Devise.setup do |config| #Replace example.com with your own domain name
     config.password_length = 8..128
     config.reset_password_within = 6.hours
     config.sign_out_via = :delete
+    #config.allow_unconfirmed_access_for = 2.hours
+
     #Add your ID and secret here #ID first, secret second
     config.omniauth :facebook, "235483700193539", "cb0b54c82d8606287dc10cc1e0386fb3", scope: 'public_profile,email', info_fields: 'email,name'
     config.omniauth :github, "16f40ebaeb6636adb36f", "a37de2577c3c913b521527e6477c71c6ab7fb43e", scope: 'public_profile,email', info_fields: 'email,name'
--- basicrails/omni/db/schema.rb
+++ basicrails/omni/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170309092639) do
+ActiveRecord::Schema.define(version: 20170313150505) do
 
   create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -37,6 +37,11 @@ ActiveRecord::Schema.define(version: 20170309092639) do
     t.string   "uid"
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
