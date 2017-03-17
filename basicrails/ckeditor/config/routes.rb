c3146dd32918cfa9610e9680bb2b07e5af6f4644
balambf <balaji.d@madebyfire.com>
2017-03-17 11:14:44 +0530

    rails_admin_editor_updated

---
+++ basicrails/ckeditor/config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  mount Ckeditor::Engine => '/ckeditor'
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  resources :articles
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
